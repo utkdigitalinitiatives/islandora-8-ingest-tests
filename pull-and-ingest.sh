3503a21002b4000f092f58af4762d4f9eccec250
bridger <bdysonsmith@gmail.com>
2019-05-16 15:06:51 -0400

    Address DIT-1224: begin adding pieces to pull a sample book

--- pull-and-ingest.sh
+++ pull-and-ingest.sh
@@ -3,9 +3,16 @@
 # variables
 URL="https://digital.lib.utk.edu/collections/islandora/object/"
 GAMBLE_PIDS=(101 102 103 104 105 106 107 108 109 110)
+# ASCOOP_PIDS are not sequential but the array will be read in order
+# book, page 1, page 2, etc, etc
+ASCOOP_PIDS=(1507160130 1507160136 1507160134 1507160131 1507160132 1507160135 1507160133)
 OBJ="/datastream/OBJ/content"
 MODS="/datastream/MODS/content"
+HOCR="/datastreams/HOCR/content"
+OCR="/datastreams/OCR/content"
+
 GAMBLE_TARGET="/tmp/gamble/"
+ASCOOP_TARGET="/tmp/ascoop/issue1/"
 
 if [[ ! -d "${GAMBLE_TARGET}" ]]; then
     mkdir "${GAMBLE_TARGET}"
